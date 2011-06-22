278b3f2ceffbf17c6564a844e13eeff557d56b56
greendog <greendog@greendog.ru>
2011-06-22 07:01:23 +0400

    searchlogic

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
+
+gem 'will_paginate'
+gem 'authlogic'
+gem 'searchlogic', :path => 'vendor/bundle/searchlogic'
+gem 'paperclip'
+gem 'prawn'
+gem 'populator'
+gem 'faker'
+# group :development, :test do
+#   gem 'webrat'
+# end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TicketMule::Application.config.secret_token = 'eb0c57995b89b8180bd6e73cb46bdbcb521b4ee7b750da79a04e869774f46cf948716229a9526a1fd7b7ec76d49f1940f5357291831d2bbaa28e8f52530d9c53'
